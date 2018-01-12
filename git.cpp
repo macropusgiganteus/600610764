64f45dfb8aca977e198a18cd6fd6f59d77e52d15
macropusgiganteus <macropusgiganteus@hotmail.com>
2018-01-12 16:39:20 +0700

    600610764

---
+++ git.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	cout<<"-----------------\n";
+	cout<<" vvvvvvvvvvvvvvv\n";
+	cout<<"  vvvvvvvvvvvvv\n";
+	cout<<"   vvvvvvvvvvv\n";
+	cout<<"    vvvvvvvvv\n";
+	cout<<"     vvvvvvv\n";
+	cout<<"      vvvvv\n";
+	cout<<"       vvv\n";
+	cout<<"        v\n";
+return 0;}
+
